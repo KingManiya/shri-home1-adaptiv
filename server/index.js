6c1e7b7524b0e2ad4e15088e42959b7833c07391
Евгений Гончаренко <KingManiya@yandex.ru>
2018-10-15 06:05:39 +0300

    На сервер добавлен пакет для cors запросов

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const registerRoutes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
