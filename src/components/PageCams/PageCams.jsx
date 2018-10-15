62041b28dd6a8ec63a2fe99a3bb5578053c4d6f6
Евгений Гончаренко <KingManiya@yandex.ru>
2018-10-15 06:06:44 +0300

    На страницы событий и камер подключены запросы на сервер за данными

--- src/components/PageCams/PageCams.jsx
+++ src/components/PageCams/PageCams.jsx
@@ -5,17 +5,29 @@
 import React from 'react'
 import style from '../PageEvents/PageEvents.scss';
 import Content from "../Content/Content";
-import camsData from "../../../data/cams";
+// import camsData from "../../../data/cams";
 import Card from "../Card/Card";
 
 export default class PageCams extends React.Component {
 
+    state = {
+        cams: [],
+    };
+
+    componentDidMount() {
+        window.fetch('http://localhost:8000/api/cams')
+            .then(response => response.json())
+            .then(eventsData => {
+                this.setState({cams: eventsData.cams})
+            })
+    }
+
     render() {
-        const cams = camsData.cams.map((cam, index) => <Card {...cam} key={index}/>);
+        const cams = this.state.cams.map((cam, index) => <Card {...cam} key={index}/>);
 
         return (
             <Content title='Камеры' className={style['normal']}>
-                {cams}
+                {cams.length ? cams : 'Загрузка...'}
             </Content>
         )
     }
--- src/components/PageEvents/PageEvents.jsx
+++ src/components/PageEvents/PageEvents.jsx
@@ -5,15 +5,29 @@
 import React from 'react'
 import Content from "../Content/Content";
 import Card from "../Card/Card";
-import eventsData from '../../../data/events'
+// import eventsData from '../../../data/events'
 import style from "./PageEvents.scss";
 
 export default class PageEvents extends React.Component {
+
+    state = {
+        events: [],
+    };
+
+    componentDidMount() {
+        window.fetch('http://localhost:8000/api/events')
+            .then(response => response.json())
+            .then(eventsData => {
+                this.setState({events: eventsData.events})
+            })
+    }
+
     render() {
-        const events = eventsData.events.map((event, index) => <Card {...event} key={index}/>);
+        const events = this.state.events.map((event, index) => <Card {...event} key={index}/>);
+
         return (
             <Content title='Лента событий' className={style['normal']}>
-                {events}
+                {events.length ? events : 'Загрузка...'}
             </Content>
         )
     }
