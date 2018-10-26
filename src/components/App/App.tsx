1f0fa1552f9a4dc75921d273fcdabee0a1e7366f
Евгений Гончаренко <KingManiya@yandex.ru>
2018-10-26 22:03:19 +0300

    tslint

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import {hot} from 'react-hot-loader'
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import {HashRouter as Router, Route} from "react-router-dom";
-// import PageEvents from "../PageEvents/PageEvents";
-// import PageCams from "../PageCams/PageCams";
-import PageDev from "../PageDev/PageDev";
+import {hot} from 'react-hot-loader';
+import {HashRouter as Router, Route} from 'react-router-dom';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+// import PageCams from '../PageCams/PageCams';
+import PageDev from '../PageDev/PageDev';
+// import PageEvents from '../PageEvents/PageEvents';
 
 const style = require('./App.scss');
 
 class App extends React.Component {
-    render() {
+    public render() {
         return (
             <Router>
                 <div className={style['normal']}>
                     <Header/>
-                    {/*<Route path='/' component={PageEvents} exact/>*/}
-                    <Route path='/info' component={PageDev}/>
-                    <Route path='/devices' component={PageDev}/>
-                    {/*<Route path='/scenarios' component={PageDev}/>*/}
-                    {/*<Route path='/cams' component={PageCams}/>*/}
+                    {/*<Route path="/" component={PageEvents} exact/>*/}
+                    <Route path="/info" component={PageDev}/>
+                    <Route path="/devices" component={PageDev}/>
+                    {/*<Route path="/scenarios" component={PageDev}/>*/}
+                    {/*<Route path="/cams" component={PageCams}/>*/}
                     <Footer/>
                 </div>
             </Router>
-        )
+        );
     }
 }
 
-export default hot(module)(App)
+export default hot(module)(App);
