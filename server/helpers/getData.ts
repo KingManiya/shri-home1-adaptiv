756c9399d9e35dbab2937f2517e5997dc4c869ab
Евгений Гончаренко <KingManiya@yandex.ru>
2018-10-28 20:43:41 +0300

    Сервер переписан на ts

--- server/helpers/getData.ts
+++ server/helpers/getData.ts
@@ -1,35 +1,43 @@
-const fs = require('fs');
-const {promisify} = require('util');
+import fs from 'fs';
+import {promisify} from 'util';
 const readFileAsync = promisify(fs.readFile);
+
+interface IEvent {
+    type: string;
+}
+
+interface IData {
+    events: IEvent[];
+}
 
 function getDataEventsFromFile() {
     return readFileAsync(__dirname + '/../../data/events.json')
-        .then(data => JSON.parse(data));
+        .then((data: Buffer) => JSON.parse(data.toString()));
 }
 
-function getDataCamsFromFile() {
+export function getDataCamsFromFile() {
     return readFileAsync(__dirname + '/../../data/cams.json')
-        .then(data => JSON.parse(data));
+        .then((data: Buffer) => JSON.parse(data.toString()));
 }
 
-function getEvents(type, offset, limit) {
+export function getEvents(type: string, offset: number, limit: number) {
     return new Promise((resolve, reject) => {
 
-        getDataEventsFromFile().then(data => {
+        getDataEventsFromFile().then((data: IData) => {
             const events = data.events;
 
             let result = events;
 
             if (type) {
-                //Типы из запроса
+                // Типы из запроса
                 const requestTypes = type.split(':');
 
-                //Типы которые есть в событиях
-                let availableTypes = new Set();
+                // Типы которые есть в событиях
+                const availableTypes = new Set();
                 events.forEach(event => availableTypes.add(event.type));
 
-                //Поиск типов, которые не доступны в данных
-                const errorTypes = requestTypes.filter(type => !availableTypes.has(type));
+                // Поиск типов, которые не доступны в данных
+                const errorTypes = requestTypes.filter((requestType: string) => !availableTypes.has(requestType));
                 if (errorTypes.length) {
                     reject({
                         status: 400,
@@ -37,11 +45,11 @@ function getEvents(type, offset, limit) {
                     });
                 }
 
-                //Фильтруем на запрошенные типы
+                // Фильтруем на запрошенные типы
                 result = events.filter(event => requestTypes.includes(event.type));
             }
 
-            //Фильтруем на ограничения
+            // Фильтруем на ограничения
             if (offset || limit) {
                 const from = offset ? offset - 1 : 0;
                 const count = limit ? from + limit : result.length;
@@ -51,9 +59,5 @@ function getEvents(type, offset, limit) {
             resolve({events: result});
         });
 
-
     });
 }
-
-module.exports.getEvents = getEvents;
-module.exports.getDataCamsFromFile = getDataCamsFromFile;
