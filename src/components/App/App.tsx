23c865d157c84584d000654045234917847a343e
Евгений Гончаренко <KingManiya@yandex.ru>
2018-11-12 14:19:59 +0300

    Card, PageEvents переведены на bem-react. Вынесен блок CardData

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import {hot} from 'react-hot-loader';
 import {HashRouter as Router, Route} from 'react-router-dom';
 // import PageCamsContainer from '../../containers/PageCamsContainer/PageCamsContainer';
-// import PageEventsContainer from '../../containers/PageEventsContainer/PageEventsContainer';
+import PageEventsContainer from '../../containers/PageEventsContainer/PageEventsContainer';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import PageDev from '../PageDev/PageDev';
@@ -18,7 +18,7 @@ class App extends React.Component {
             <Router>
                 <div className={app()}>
                     <Header/>
-                    {/*<Route path="/" component={PageEventsContainer} exact/>*/}
+                    <Route path="/" component={PageEventsContainer} exact/>
                     <Route path="/info" component={PageDev}/>
                     <Route path="/devices" component={PageDev}/>
                     {/*<Route path="/cams" component={PageCamsContainer}/>*/}
